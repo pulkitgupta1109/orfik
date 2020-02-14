a0840261f046a45448b8a3db5324bc6278406bb4
arjoonn sharma <arjoonn.94@gmail.com>
2020-02-14 10:51:50 +0530

    don't commit public

--- orfik/__main__.py
+++ orfik/__main__.py
@@ -4,8 +4,11 @@ from orfik import static
 
 parser = argparse.ArgumentParser()
 parser.add_argument("cmd")
+parser.add_argument("--api-base", default="https://orfikapi.compsoc.club")
+parser.add_argument("--target-dir", default="public")
+parser.add_argument("--templates-dir", default="templates")
 args = parser.parse_args()
 
 if args.cmd == "build":
-    os.makedirs("public", exist_ok=True)
-    static.build()
+    os.makedirs(args.target_dir, exist_ok=True)
+    static.build(args)
--- orfik/static.py
+++ orfik/static.py
@@ -8,19 +8,20 @@ from jinja2 import Environment, FileSystemLoader, Template
 from orfik.questions import questions
 
 
-def build(templates_dir="templates", target="public"):
-    target = Path(target)
+def build(args):
+    templates_dir = Path(args.templates_dir)
+    target_dir = Path(args.target_dir)
     env = Environment(loader=FileSystemLoader(templates_dir))
-    html = env.get_template("index.html").render()
-    with open(target / "index.html", "w") as fl:
+    html = env.get_template("index.html").render(api_base=args.api_base)
+    with open(target_dir / "index.html", "w") as fl:
         fl.write(htmlmin.minify(html))
     # ---------------
     previous_hashes = [sha512("orfik".encode()).hexdigest()]
     for q in questions:
         tpl = env.get_template("question.html")
-        html = htmlmin.minify(tpl.render(q=q))
+        html = htmlmin.minify(tpl.render(q=q, api_base=args.api_base))
         for previous_hash in previous_hashes:
-            with open(target / f"{previous_hash}.html", "w") as fl:
+            with open(target_dir / f"{previous_hash}.html", "w") as fl:
                 fl.write(html)
         previous_hashes = [sha512(ans.encode()).hexdigest() for ans in q.answers]
     # ----------------
