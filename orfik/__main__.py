47b88fdd7e615a2e5df15e3447bac3408896f272
arjoonn sharma <arjoonn.94@gmail.com>
2020-04-11 23:26:33 +0530

    update netlify url

--- orfik/__main__.py
+++ orfik/__main__.py
@@ -5,7 +5,7 @@ from orfik import static, dynamic, questions
 parser = argparse.ArgumentParser()
 parser.add_argument("cmd")
 parser.add_argument("--port", default=8080, type=int)
-parser.add_argument("--api-base", default="https://orfikapi.compsoc.club")
+parser.add_argument("--api-base", default="https://orfikapi.herokuapp.com")
 parser.add_argument("--target-dir", default="public")
 parser.add_argument("--templates-dir", default="templates")
 parser.add_argument("--questions-dir", default="questions")
